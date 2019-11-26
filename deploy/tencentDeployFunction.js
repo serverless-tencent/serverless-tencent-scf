e07be42e0fd335b5d160fca2dabd6ec48794287b
dfounderliu <dfounderliu@tencent.com>
2019-11-26 19:33:50 +0800

    fix function status

--- deploy/tencentDeployFunction.js
+++ deploy/tencentDeployFunction.js
@@ -97,7 +97,7 @@ class TencentDeployFunction {
     let status = 'Updating'
     let times = 90
     while (status == 'Updating' || status == 'Creating') {
-      const tempFunc = await this.getFunction('default', funcObject.FuncName)
+      const tempFunc = await func.getFunction('default', funcObject.FuncName)
       status = tempFunc.Status
       await utils.sleep(1000)
       times = times - 1
